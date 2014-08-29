0df64ca6052ffb50788ffcabafbc65ad8785e6e1
John van de Water <john.vd.water85@gmail.com>
2014-08-30 00:59:26 +0200

    Add solution

--- Spel.js
+++ Spel.js
@@ -1,3 +1,21 @@
 Speelveld.prototype.klik = function(vakje) {
   // De speler heeft op een vakje gelikt, wat nu?
+
+  if(this.isDitVakjeNogVrij(vakje)) {
+
+    if (this.wieIsDeHuidigeSpeler() == 'x') {
+      this.tekenX(vakje);
+    }
+
+    if (this.wieIsDeHuidigeSpeler() == 'o') {
+      this.tekenO(vakje);
+    }
+
+    if(this.heeftErIemandGewonnen()) {
+      this.benoemWinnaar();
+    }
+
+    this.wisselVanSpeler();
+
+  }
 }
